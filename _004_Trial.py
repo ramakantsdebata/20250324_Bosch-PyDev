3bafbbbbbae6acb00f5a58d6ff32629d00a9147d
Ramakant Debata <ramakant.s.debata@gmail.com>
2025-03-25 13:09:17 +0530

    Day 02

---
+++ _004_Trial.py
@@ -0,0 +1,6 @@
+from fractions import Fraction
+PI = Fraction("22/7")
+PI = Fraction(22,7)
+
+print(PI)
+print(type(PI))
---
+++ _005_ControlStructs.py
@@ -0,0 +1,39 @@
+# from random import randint
+
+# num = randint(1, 100)
+# print(num)
+# if num%2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+# if num > 50:
+#     print("High")
+# elif num < 50:
+#     print("Low")
+# else:
+#     print("Equal")
+
+
+# from random import randint
+
+# num = randint(1, 7)
+# print(num)
+
+# match(num):
+#     case 1:
+#         print("One")
+#     case 2:
+#         print("Two")
+#     case 3:
+#         print("Three")
+#     case 4:
+#         print("Four")
+#     case 5:
+#         print("Five")
+#     case _:
+#         print("Other Number")
+
+
+## Loops
