3da3faafb1415769e1cc9a5c937ff84e39241246
Ramakant Debata <ramakant.s.debata@gmail.com>
2025-03-26 10:22:46 +0530

    Day 02

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # 20250324_Bosch-PyDev
 
+## Further Reading
+* SOLID Principles
+
 ## Links
 * [PEP Index](https://peps.python.org/)
 * [PEP-8: Style Guide for Python](https://peps.python.org/pep-0008/)
 * [PEP-20: Zen of Python](https://peps.python.org/pep-0020/)
+* [Lexical Analysis](https://docs.python.org/3/reference/lexical_analysis.html#lexical-analysis)
+    * [Escape Sequences](https://docs.python.org/3/reference/lexical_analysis.html#escape-sequences)
+    * [Format String](https://docs.python.org/3/library/string.html#format-string-syntax)
---
+++ _007_String.py
@@ -0,0 +1,74 @@
+# Quotes (Single, Double, Triple)
+s1 = "This is Vinay's car"
+s2 = 'He say, "Let us go!!"'
+print(s1)
+print(s2)
+
+comment = """This is
+a 
+multiline
+string."""
+print(comment)
+
+help = '''
+Usage: asjhakfhdas
+Syntax: ljaskjlhsdfj
+
+Switches:
+a: adsfads
+b: asdfasdf
+c: asdfasd'''
+print(help)
+
+str1 = "Component""Feature"
+print(str1)
+
+
+## Format strings
+tech = "Python"
+duration = 5
+s1 = "This is a Python Training for 5 days"
+s2 = "This is a {} training for {} days"
+s3 = "This is a %s training for %d days"
+s4 = f"This is a {tech} training for {duration} days"
+print(s1)
+print(s2.format(tech, duration))
+print(s3%(tech, duration))
+print(s4)
+
+x = 10
+y = 20
+print("x =", x)
+print(f"x = {x}")
+print(f"{x=}, {y=}")
+
+## Accessing the data of strings
+s1 = "This is a Python Training for 5 days."
+print(s1[10])
+print(s1[10:15])
+print(s1[10:16])
+print(s1[10:])
+print(s1[:16])
+print(s1[:])
+print(s1[-1:])
+print(s1[-5:])
+print(s1[-7:-1])
+print(s1[-7:5])
+print(s1[-7:], s1[:5])
+print(s1[-7:], s1[:5], sep="")
+
+print(f"%d%d"%(x, y))
+print(x, y)
+print(x, y, sep="")
+
+
+print("="*20)
+str = "1234567890"
+print(str[:6])
+print(str[:6:2])
+print(str[1:6:2])
+print(str[::2])
+print(str[1::2])
+print(str[::-1])
+print(s1[::-1])
+print(s1[-7:-27:-1])
