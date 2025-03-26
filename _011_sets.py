fee37685d8d279ad402f065d6a329047af897055
Ramakant Debata <ramakant.s.debata@gmail.com>
2025-03-26 15:02:02 +0530

    Day 03

--- _011_sets.py
+++ _011_sets.py
@@ -1,4 +1,4 @@
-# Mutable collection of 'keys'
+# Mutable collection of non-sequential 'keys'
 # key --> hashable --> immutable
 data = [1, 2, 3, "Python"]  #, {1, 2, 3}]
 # print(hash(data))
@@ -10,3 +10,60 @@ st4 = {1, 2, 3, 4}
 st4.add(5)
 
 print(st1, st2, st3, st4, sep="\n")
+
+st5 = {3, 4, 5, 6}
+st5.update(st4)
+print(f"{st5=}")
+
+print("="* 40, "\n")
+
+st1 = {'a', 'b', 'c', 'd'}
+st2 = {'c', 'd', 'e', 'f'}
+st3 = set(st2)
+print(f"{st1=}")
+print(f"{st2=}")
+print(f"{st3=}")
+for k in st3:
+    if k == 'd':
+        print(id(k))
+        
+st3.update(st1)
+print(f"{st3=}")
+
+# print('c' in st2)
+for k in st3:
+    if k == 'd':
+        print(id(k))
+
+print(f"{st1=}")
+print(f"{st2=}")
+print(st1.intersection(st2))
+print(st1 | st2)
+print(st1 & st2) #st1.intersection(st2)
+print(st1 - st2) #st1.difference(st2)
+print(st1 ^ st2) #st1.symmetric_difference(st2)
+st4 = {'d'}
+print(st4<=st2) #st4.issubset(st2)
+
+st4.add('g')
+st4.remove('d') # returns None; removes item if present and error if not
+# st4.remove('d') # returns None; removes item if present and error if not
+st4.discard('d') # returns the popped element
+
+st4 = set(st1)
+while len(st4):
+    print("-->", st4.pop())
+
+st2.clear()
+
+fs1 = frozenset(st1)
+print(f"{fs1=}")
+
+st1 = {1, 2, 3, 4}
+st2 = {5, 6, 7}
+
+print(st1 <= st2)
+print(st1 >= st2)
+print(st1.isdisjoint(st2))
+
+print((st1 & st2) == set())
---
+++ _012_dictionaries.py
@@ -0,0 +1,47 @@
+# Mutable non-seq collection of key-value pairs
+dt1 = {'key': 'value'}
+print(type(dt1), dt1)
+
+dt2 = {'key': [1, 2, 3]}
+# dt3 = {{'a', 'b', 'c'}: [1, 2, 3]}
+
+key_1 = frozenset({'heated', 'medium', 'reinforced'})
+cont = {key_1: "India to Bhutan"}
+print(f"{cont=}")
+print('heated' in key_1)
+
+dt4 = dict.fromkeys({'a', 'b', 'c', 'd'})
+print(f"{dt4=}")
+dt4["b"] = "bat"
+print(f"{dt4=}")
+
+# print(dt4["e"])
+
+if "e" in dt4:
+    print(dt4["e"])
+else:
+    print("data not found")
+
+# val = dt4.get("e")
+# if val:
+#     print(val)
+# else:
+#     print("data not found")
+
+# x = 1
+# z = y = x
+# print(x, y, z)
+
+# if dt4.get("e"):
+if val := dt4.get("b"):
+    print(val)
+else:
+    print("data not found")
+
+emp = {
+    1: "Manish",
+    2: "Abhijeet",
+    3: "Rakesh"
+}
+
+print(f"{emp=}")
